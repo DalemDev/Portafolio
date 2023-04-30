bfffa2367a842d15dfa66f20e31307f9583c59c5
dalemdev <developerj659@gmail.com>
2023-04-29 20:29:34 -0500

    implementando envio de email

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,20 +1,57 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../css/Formulario.css";
 import { ANIMACION } from "./Archivos";
 import Aos from "aos";
 import "aos/dist/aos.css";
-
-const sendEmail = (e) => {
-  e.preventDefault();
-};
+import emailjs from "emailjs-com";
 
 function Formulario() {
+  const [mail, setMail] = useState({
+    nombre: "",
+    correo: "",
+    mensaje: "",
+  });
+
+  const [notificacion, setNotificacion] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_dc9cwjr",
+        "template_1osifg3",
+        e.target,
+        "TWnIiqjHaFNU-TEo5"
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          setNotificacion("Mensaje enviado correctamente ✅");
+        } else {
+          setNotificacion(res.text);
+        }
+        setMail({ nombre: "", correo: "", mensaje: "" });
+      })
+      .catch((err) => {
+        setNotificacion(err.message + " ❌");
+      });
+
+    setTimeout(() => {
+      setNotificacion("");
+    }, 3000);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setMail({ ...mail, [name]: value });
+  };
+
   useEffect(() => {
     Aos.init();
   }, []);
 
   return (
     <form className="form" onSubmit={sendEmail} data-aos="fade-up">
+      {notificacion && <p className="notificacion">{notificacion}</p>}
       <div className="imagen-form">
         <img
           className="form-imagen"
@@ -30,6 +67,8 @@ function Formulario() {
               placeholder=" "
               id="nombre"
               name="nombre"
+              onChange={handleChange}
+              value={mail.nombre}
               required
             />
             <label className="form__label">Nombre</label>
@@ -39,8 +78,10 @@ function Formulario() {
               type="email"
               className="form__input"
               placeholder=" "
-              id="correo"
+              id="email"
               name="correo"
+              value={mail.correo}
+              onChange={handleChange}
               required
             />
             <label className="form__label">Correo</label>
@@ -53,6 +94,8 @@ function Formulario() {
               cols="30"
               rows="1"
               name="mensaje"
+              value={mail.mensaje}
+              onChange={handleChange}
               required
             ></textarea>
             <label className="form__label">Mensaje</label>
